23849208362ac4b1a1d6f7b213736aa3ec398fd5
Florian Weber <fweber@Florians-MacBook-Pro.local>
2019-04-04 10:03:11 +0200

    feat: added EmailLink component

---
+++ src/components/EmailLink.vue
@@ -0,0 +1,28 @@
+<template>
+    <a href="" @click.prevent="sendMail" :aria-label="`E-Mail senden an ${email}`">
+        <span>
+            {{ splittedEmail[0] }}<span class="d-none"></span>@<span class="d-none"></span>{{ splittedEmail[1] }}
+        </span>
+    </a>
+</template>
+
+<script>
+    export default {
+        props: {
+            email: String
+        },
+        data() {
+            return {
+                splittedEmail: ''
+            }
+        },
+        mounted() {
+            this.splittedEmail = this.email.split('@');
+        },
+        methods: {
+            sendMail() {
+                window.location.href = `mailto:${this.email}`;
+            }
+        }
+    }
+</script>
