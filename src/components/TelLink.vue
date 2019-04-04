dd465110c49cbdc979b047285202c4ed5ad3049d
Florian Weber <fweber@Florians-MacBook-Pro.local>
2019-04-04 10:03:26 +0200

    feat: added TelLink component

---
+++ src/components/TelLink.vue
@@ -0,0 +1,22 @@
+<template>
+    <a href="" @click.prevent="call" :aria-label="`Die Nummer ${tel} anrufen`">
+        <span v-text="tel" v-if="!wrapper"></span>
+        <slot></slot>
+    </a>
+</template>
+
+<script>
+    export default {
+        props: {
+            tel: String,
+            wrapper: Boolean
+        },
+        methods: {
+            call() {
+                // eslint-disable-next-line no-useless-escape
+                const tel = this.tel.replace(/[\s\/-]/g, '');
+                window.location.href = `tel:${tel}`;
+            }
+        }
+    }
+</script>
