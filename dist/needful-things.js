7888e086cfbf8f994022c200c9f8d860d20639b8
Florian Weber <fweber@Florians-MacBook-Pro.local>
2019-04-04 10:07:23 +0200

    chore: built for version 1.0.0

---
+++ dist/needful-things.js
@@ -0,0 +1,238 @@
+//
+//
+//
+//
+//
+//
+//
+//
+var script = {
+  props: {
+    email: String
+  },
+  data: function data() {
+    return {
+      splittedEmail: ''
+    };
+  },
+  mounted: function mounted() {
+    this.splittedEmail = this.email.split('@');
+  },
+  methods: {
+    sendMail: function sendMail() {
+      window.location.href = "mailto:".concat(this.email);
+    }
+  }
+};
+
+function normalizeComponent(template, style, script, scopeId, isFunctionalTemplate, moduleIdentifier
+/* server only */
+, shadowMode, createInjector, createInjectorSSR, createInjectorShadow) {
+  if (typeof shadowMode !== 'boolean') {
+    createInjectorSSR = createInjector;
+    createInjector = shadowMode;
+    shadowMode = false;
+  } // Vue.extend constructor export interop.
+
+
+  var options = typeof script === 'function' ? script.options : script; // render functions
+
+  if (template && template.render) {
+    options.render = template.render;
+    options.staticRenderFns = template.staticRenderFns;
+    options._compiled = true; // functional template
+
+    if (isFunctionalTemplate) {
+      options.functional = true;
+    }
+  } // scopedId
+
+
+  if (scopeId) {
+    options._scopeId = scopeId;
+  }
+
+  var hook;
+
+  if (moduleIdentifier) {
+    // server build
+    hook = function hook(context) {
+      // 2.3 injection
+      context = context || // cached call
+      this.$vnode && this.$vnode.ssrContext || // stateful
+      this.parent && this.parent.$vnode && this.parent.$vnode.ssrContext; // functional
+      // 2.2 with runInNewContext: true
+
+      if (!context && typeof __VUE_SSR_CONTEXT__ !== 'undefined') {
+        context = __VUE_SSR_CONTEXT__;
+      } // inject component styles
+
+
+      if (style) {
+        style.call(this, createInjectorSSR(context));
+      } // register component module identifier for async chunk inference
+
+
+      if (context && context._registeredComponents) {
+        context._registeredComponents.add(moduleIdentifier);
+      }
+    }; // used by ssr in case component is cached and beforeCreate
+    // never gets called
+
+
+    options._ssrRegister = hook;
+  } else if (style) {
+    hook = shadowMode ? function () {
+      style.call(this, createInjectorShadow(this.$root.$options.shadowRoot));
+    } : function (context) {
+      style.call(this, createInjector(context));
+    };
+  }
+
+  if (hook) {
+    if (options.functional) {
+      // register for functional component in vue file
+      var originalRender = options.render;
+
+      options.render = function renderWithStyleInjection(h, context) {
+        hook.call(context);
+        return originalRender(h, context);
+      };
+    } else {
+      // inject component registration as beforeCreate hook
+      var existing = options.beforeCreate;
+      options.beforeCreate = existing ? [].concat(existing, hook) : [hook];
+    }
+  }
+
+  return script;
+}
+
+var normalizeComponent_1 = normalizeComponent;
+
+/* script */
+var __vue_script__ = script;
+/* template */
+
+var __vue_render__ = function __vue_render__() {
+  var _vm = this;
+
+  var _h = _vm.$createElement;
+
+  var _c = _vm._self._c || _h;
+
+  return _c("a", {
+    attrs: {
+      href: "",
+      "aria-label": "E-Mail senden an " + _vm.email
+    },
+    on: {
+      click: function click($event) {
+        $event.preventDefault();
+        return _vm.sendMail($event);
+      }
+    }
+  }, [_c("span", [_vm._v("\n        " + _vm._s(_vm.splittedEmail[0])), _c("span", {
+    staticClass: "d-none"
+  }), _vm._v("@"), _c("span", {
+    staticClass: "d-none"
+  }), _vm._v(_vm._s(_vm.splittedEmail[1]) + "\n    ")])]);
+};
+
+var __vue_staticRenderFns__ = [];
+__vue_render__._withStripped = true;
+/* style */
+
+var __vue_inject_styles__ = undefined;
+/* scoped */
+
+var __vue_scope_id__ = undefined;
+/* module identifier */
+
+var __vue_module_identifier__ = undefined;
+/* functional template */
+
+var __vue_is_functional_template__ = false;
+/* style inject */
+
+/* style inject SSR */
+
+var EmailLink = normalizeComponent_1({
+  render: __vue_render__,
+  staticRenderFns: __vue_staticRenderFns__
+}, __vue_inject_styles__, __vue_script__, __vue_scope_id__, __vue_is_functional_template__, __vue_module_identifier__, undefined, undefined);
+
+//
+//
+//
+//
+//
+//
+//
+var script$1 = {
+  props: {
+    tel: String,
+    wrapper: Boolean
+  },
+  methods: {
+    call: function call() {
+      // eslint-disable-next-line no-useless-escape
+      var tel = this.tel.replace(/[\s\/-]/g, '');
+      window.location.href = "tel:".concat(tel);
+    }
+  }
+};
+
+/* script */
+var __vue_script__$1 = script$1;
+/* template */
+
+var __vue_render__$1 = function __vue_render__() {
+  var _vm = this;
+
+  var _h = _vm.$createElement;
+
+  var _c = _vm._self._c || _h;
+
+  return _c("a", {
+    attrs: {
+      href: "",
+      "aria-label": "Die Nummer " + _vm.tel + " anrufen"
+    },
+    on: {
+      click: function click($event) {
+        $event.preventDefault();
+        return _vm.call($event);
+      }
+    }
+  }, [!_vm.wrapper ? _c("span", {
+    domProps: {
+      textContent: _vm._s(_vm.tel)
+    }
+  }) : _vm._e(), _vm._v(" "), _vm._t("default")], 2);
+};
+
+var __vue_staticRenderFns__$1 = [];
+__vue_render__$1._withStripped = true;
+/* style */
+
+var __vue_inject_styles__$1 = undefined;
+/* scoped */
+
+var __vue_scope_id__$1 = undefined;
+/* module identifier */
+
+var __vue_module_identifier__$1 = undefined;
+/* functional template */
+
+var __vue_is_functional_template__$1 = false;
+/* style inject */
+
+/* style inject SSR */
+
+var TelLink = normalizeComponent_1({
+  render: __vue_render__$1,
+  staticRenderFns: __vue_staticRenderFns__$1
+}, __vue_inject_styles__$1, __vue_script__$1, __vue_scope_id__$1, __vue_is_functional_template__$1, __vue_module_identifier__$1, undefined, undefined);
+
+export { EmailLink, TelLink };
