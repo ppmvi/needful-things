ffe95b44a9d453525f1b8c28d74d732b77f86f35
Florian Weber <fweber@Florians-MacBook-Pro.local>
2019-04-04 10:02:52 +0200

    initial commit

---
+++ rollup.config.js
@@ -0,0 +1,41 @@
+import commonjs from 'rollup-plugin-commonjs' ;
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import vue from 'rollup-plugin-vue';
+import { terser } from 'rollup-plugin-terser';
+
+const plugins = [
+    resolve(),
+    commonjs(),
+    vue({
+        compileTemplate: true
+    }),
+    babel({
+        runtimeHelpers: true,
+        extensions: ['.js', '.jsx', '.es6', '.es', '.mjs', '.vue'],
+        babelrc: false,
+        presets: [['@babel/preset-env', { modules: false }]],
+    })
+];
+const minPlugins = [
+    ...plugins,
+    terser()
+]
+
+const esm = {
+    input: 'src/index.js',
+    output: {
+        format: 'esm',
+        file: 'dist/needful-things.js'
+    },
+    plugins
+};
+
+const esmMin = JSON.parse(JSON.stringify(esm));
+esmMin.output.file = 'dist/needful-things.min.js';
+esmMin.plugins = minPlugins
+
+export default [
+    esm,
+    esmMin
+];
