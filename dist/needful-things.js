91aac8abc04ef942f4fde7fac1974541d86abcc1
Florian Weber <fweber@Florians-MacBook-Pro.local>
2019-04-05 09:51:58 +0200

    "chore: Adding newly built files for version 1.1.0"

--- dist/needful-things.js
+++ dist/needful-things.js
@@ -252,4 +252,189 @@ var TelLink = normalizeComponent_1({
   staticRenderFns: __vue_staticRenderFns__$1
 }, __vue_inject_styles__$1, __vue_script__$1, __vue_scope_id__$1, __vue_is_functional_template__$1, __vue_module_identifier__$1, undefined, undefined);
 
-export { EmailLink, TelLink };
+function applyFacebookMetaTags() {
+  var meta = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+  var _meta$type = meta.type,
+      type = _meta$type === void 0 ? '' : _meta$type,
+      _meta$title = meta.title,
+      title = _meta$title === void 0 ? '' : _meta$title,
+      _meta$description = meta.description,
+      description = _meta$description === void 0 ? '' : _meta$description,
+      _meta$image = meta.image,
+      image = _meta$image === void 0 ? '' : _meta$image,
+      _meta$siteName = meta.siteName,
+      siteName = _meta$siteName === void 0 ? '' : _meta$siteName;
+  return [type && {
+    property: 'og:type',
+    content: type
+  }, title && {
+    property: 'og:title',
+    content: title
+  }, description && {
+    property: 'og:description',
+    content: description
+  }, image && {
+    property: 'og:image',
+    content: image
+  }, siteName && {
+    property: 'og:site_name',
+    content: siteName
+  }].filter(function (tag) {
+    return tag;
+  });
+}
+function applyTwitterMetaTags() {
+  var meta = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+  var _meta$title2 = meta.title,
+      title = _meta$title2 === void 0 ? '' : _meta$title2,
+      _meta$description2 = meta.description,
+      description = _meta$description2 === void 0 ? '' : _meta$description2,
+      _meta$image2 = meta.image,
+      image = _meta$image2 === void 0 ? '' : _meta$image2,
+      _meta$site = meta.site,
+      site = _meta$site === void 0 ? '' : _meta$site,
+      _meta$creator = meta.creator,
+      creator = _meta$creator === void 0 ? '' : _meta$creator;
+  return [title && {
+    property: 'twitter:title',
+    content: title
+  }, description && {
+    property: 'twitter:description',
+    content: description
+  }, image && {
+    property: 'twitter:image',
+    content: image
+  }, site && {
+    property: 'twitter:site',
+    content: site
+  }, creator && {
+    property: 'twitter:creator',
+    content: creator
+  }].filter(function (tag) {
+    return tag;
+  });
+}
+
+function _toConsumableArray(arr) {
+  return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread();
+}
+
+function _arrayWithoutHoles(arr) {
+  if (Array.isArray(arr)) {
+    for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) arr2[i] = arr[i];
+
+    return arr2;
+  }
+}
+
+function _iterableToArray(iter) {
+  if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter);
+}
+
+function _nonIterableSpread() {
+  throw new TypeError("Invalid attempt to spread non-iterable instance");
+}
+
+function applySplashscreenMetaTags() {
+  var url = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '/static/splashscreens';
+  var additional = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : [];
+  var splashscreens = [];
+  var rel = 'apple-touch-startup-image';
+  var screens = [{
+    width: 320,
+    height: 568,
+    ratio: 2,
+    orientation: 'portrait',
+    href: "".concat(url, "/iphone5_splash.png")
+  }, {
+    width: 375,
+    height: 667,
+    ratio: 2,
+    orientation: 'portrait',
+    href: "".concat(url, "/iphone6_splash.png")
+  }, {
+    width: 621,
+    height: 1104,
+    ratio: 3,
+    orientation: 'portrait',
+    href: "".concat(url, "/iphoneplus_splash.png")
+  }, {
+    width: 375,
+    height: 812,
+    ratio: 3,
+    orientation: 'portrait',
+    href: "".concat(url, "/iphonex_splash.png")
+  }, {
+    width: 414,
+    height: 896,
+    ratio: 2,
+    orientation: 'portrait',
+    href: "".concat(url, "/iphonexr_splash.png")
+  }, {
+    width: 414,
+    height: 896,
+    ratio: 3,
+    orientation: 'portrait',
+    href: "".concat(url, "/iphonexsmax_splash.png")
+  }, {
+    width: 768,
+    height: 1024,
+    ratio: 2,
+    orientation: 'portrait',
+    href: "".concat(url, "/ipad_splash.png")
+  }, {
+    width: 834,
+    height: 1112,
+    ratio: 2,
+    orientation: 'portrait',
+    href: "".concat(url, "/ipadpro1_splash.png")
+  }, {
+    width: 834,
+    height: 1194,
+    ratio: 2,
+    orientation: 'portrait',
+    href: "".concat(url, "/ipadpro3_splash.png")
+  }, {
+    width: 1024,
+    height: 1366,
+    ratio: 2,
+    orientation: 'portrait',
+    href: "".concat(url, "/ipadpro2_splash.png")
+  }].concat(_toConsumableArray(additional));
+  var _iteratorNormalCompletion = true;
+  var _didIteratorError = false;
+  var _iteratorError = undefined;
+
+  try {
+    for (var _iterator = screens[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+      var _step$value = _step.value,
+          width = _step$value.width,
+          height = _step$value.height,
+          ratio = _step$value.ratio,
+          orientation = _step$value.orientation,
+          href = _step$value.href;
+      splashscreens.push({
+        rel: rel,
+        media: "(device-width: ".concat(width, "px) and (device-height: ").concat(height, "px) and (-webkit-device-pixel-ratio: ").concat(ratio, ") and (orientation: ").concat(orientation, ")"),
+        href: href
+      });
+    }
+  } catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion && _iterator["return"] != null) {
+        _iterator["return"]();
+      }
+    } finally {
+      if (_didIteratorError) {
+        throw _iteratorError;
+      }
+    }
+  }
+
+  return splashscreens;
+}
+
+export { EmailLink, TelLink, applyFacebookMetaTags, applySplashscreenMetaTags, applyTwitterMetaTags };
