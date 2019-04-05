32d390e674e453765ee01853b302492c98869903
Florian Weber <fweber@Florians-MacBook-Pro.local>
2019-04-05 09:30:48 +0200

    refactor: renamed meta.js to social.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import EmailLink from './components/EmailLink.vue';
 import TelLink from './components/TelLink.vue';
-import { applyFacebookMetaTags, applyTwitterMetaTags } from './utils/meta';
+import { applyFacebookMetaTags, applyTwitterMetaTags } from './utils/social';
 
 export {
   EmailLink,
