8f4225959486342c9aebbe4196d562998aea84dd
Florian Weber <fweber@Florians-MacBook-Pro.local>
2019-04-08 10:28:38 +0200

    build: updated dev workflow
    Added connect and serve-static packages. The server is started when running the dev script and can be accessed by http://localhost:8080/dev/.

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,6 +1,15 @@
 #!/usr/bin/env node -r esm
 
 import Release from './release.js';
+import connect from 'connect';
+import serveStatic from 'serve-static';
+import path from 'path';
 
 const release = new Release();
 release.build(true);
+
+connect()
+    .use(serveStatic(path.resolve(__dirname, '../')))
+    .listen(8080, function() {
+      console.log('Server running on 8080...');
+    });
