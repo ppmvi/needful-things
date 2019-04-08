3338e61aa50c68bc0a6c5a770c7e4bf130645add
Florian Weber <fweber@Florians-MacBook-Pro.local>
2019-04-08 10:29:35 +0200

    feat: added applyFaviconLinks and applyFaviconMetaTags methods.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import EmailLink from './components/EmailLink.vue';
 import TelLink from './components/TelLink.vue';
 import { applyFacebookMetaTags, applyTwitterMetaTags } from './utils/social';
 import { applySplashscreenLinks } from './utils/splashscreen';
+import { applyFaviconLinks, applyFaviconMetaTags } from './utils/favicons';
 
 export {
   EmailLink,
   TelLink,
   applyFacebookMetaTags,
   applyTwitterMetaTags,
-  applySplashscreenLinks
+  applySplashscreenLinks,
+  applyFaviconLinks,
+  applyFaviconMetaTags
 };
---
+++ src/utils/favicons.js
@@ -0,0 +1,35 @@
+export function applyFaviconMetaTags(options = {}) {
+  const {
+    url = '/static/icons',
+    manifest: {
+      url: manifestUrl = '/static',
+      name: manifestName = 'manifest.json'
+    },
+    color = '#FFFFFF'
+  } = options;
+
+  return [
+    { rel: 'apple-touch-icon', sizes: '180x180', href: `${url}/apple-touch-icon.png` },
+    { rel: 'icon', type: 'image/png', href: `${url}/favicon-32x32.png`, sizes: '32x32' },
+    { rel: 'icon', type: 'image/png', href: `${url}/favicon-16x16.png`, sizes: '16x16' },
+    { rel: 'manifest', href: `${manifestUrl}/${manifestName}` },
+    { rel: 'mask-icon', href: `${url}/safari-pinned-tab.svg`, color: color },
+    { rel: 'shortcut icon', href: `${url}/favicon.ico` }
+  ];
+}
+
+export function applyFaviconLinks(options = {}) {
+  const {
+    url = '/static/icons',
+    color = '#FFFFFF',
+    statusBarStyle = 'default'
+  } = options;
+
+  return [
+    { name: 'msapplication-TileColor', content: color },
+    { name: 'msapplication-config', content: `${url}/browserconfig.xml` },
+    { name: 'theme-color', content: color },
+    { name: 'apple-mobile-web-app-status-bar-style', content: statusBarStyle },
+    { name: 'apple-mobile-web-app-capable', content: 'yes' }
+  ];
+}
