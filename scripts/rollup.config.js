c0a4c39b85bd136d99becea24fdc7f948eb9d59b
Florian Weber <fweber@Florians-MacBook-Pro.local>
2019-04-10 11:28:14 +0200

    build: updated rollup config file to export class

--- scripts/rollup.config.js
+++ scripts/rollup.config.js
@@ -3,45 +3,90 @@ import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import vue from 'rollup-plugin-vue';
 import { terser } from 'rollup-plugin-terser';
+import license from 'rollup-plugin-license';
+import defaultsDeep from 'lodash/defaultsDeep';
+import fs from 'fs';
 
-const plugins = [
-  resolve(),
-  commonjs(),
-  vue({
-    compileTemplate: true
-  }),
-  babel({
-    runtimeHelpers: true,
-    extensions: ['.js', '.jsx', '.es6', '.es', '.mjs', '.vue'],
-    babelrc: false,
-    presets: [['@babel/preset-env', { modules: false }]]
-  })
-];
-const minPlugins = [
-  ...plugins,
-  terser()
-];
+export default class RollupConfig {
+  constructor(options = {}) {
+    this.options = defaultsDeep({}, options, {
+      minify: false,
+      name: 'needful-things',
+      input: './src/index.js',
+      external: [
+        'fs',
+        'path',
+        'ora',
+        'rfg-api',
+        'pify'
+      ]
+    });
+    this.pkg = JSON.parse(fs.readFileSync('./package.json'));
+  }
 
-const esm = {
-  input: './src/index.js',
-  output: {
-    format: 'esm',
-    file: 'dist/needful-things.js'
-  },
-  external: [
-    'fs',
-    'path',
-    'ora',
-    'rfg-api'
-  ],
-  plugins
-};
+  output() {
+    let min = '';
 
-const esmMin = JSON.parse(JSON.stringify(esm));
-esmMin.output.file = './dist/needful-things.min.js';
-esmMin.plugins = minPlugins;
+    if (this.options.minify) min = `.min`;
 
-export default [
-  esm,
-  esmMin
-];
+    return {
+      format: 'esm',
+      preferConst: true,
+      file: `dist/${this.options.name}${min}.js`
+    };
+  }
+
+  plugins() {
+    const plugins = [];
+
+    plugins.push(
+      resolve({
+        only: [
+          'lodash'
+        ]
+      })
+    );
+
+    plugins.push(commonjs());
+
+    plugins.push(
+      vue({
+        compileTemplate: true
+      })
+    );
+
+    plugins.push(
+      babel({
+        runtimeHelpers: true,
+        extensions: ['.js', '.jsx', '.es6', '.es', '.mjs', '.vue'],
+        babelrc: false,
+        presets: [['@babel/preset-env', { modules: false }]]
+      })
+    );
+
+    if (this.options.minify) plugins.push(terser());
+
+    plugins.push(
+      license({
+        banner: [
+          '/*!',
+          ` * Needful things v${this.pkg.version}`,
+          ` * Copyright <%= moment().format('YYYY') %> Florian Weber - ppm visuals & internet GmbH`,
+          ` * Released under the MIT License.`,
+          '*/'
+        ].join('\n')
+      })
+    );
+
+    return plugins;
+  }
+
+  config() {
+    return {
+      input: this.options.input,
+      output: this.output(),
+      external: this.options.external,
+      plugins: this.plugins()
+    };
+  }
+}
