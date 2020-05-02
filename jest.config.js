f86564384e6c3ad39c4e29a2b64e171e182ce354
Florian Weber <fl-weber@t-online.de>
2020-05-02 15:12:52 +0200

    test: added jest

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+  clearMocks: true,
+  coverageDirectory: './coverage/',
+  collectCoverage: true,
+  moduleFileExtensions: [
+    'js', 'json', 'ts', 'vue'
+  ],
+  transform: {
+    ".*\\.(vue)$": "vue-jest",
+    "^.+\\.ts$": "ts-jest"
+  },
+  snapshotSerializers: ['jest-serializer-vue'],
+  moduleDirectories: [
+    'node_modules'
+  ],
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$"
+};
